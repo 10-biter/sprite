d1fc2c59f560de6acbd8a197c753772ba2c72ace
10-biter <ep.student@hotmail.com>
2025-11-27 19:47:54 +0300

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+namespace sprites {
+    //%color=#6f8087 icon=\uf135
+    //%groups=["move"]
+    
 
+
+    //%block="walk to X: $x Y: $y"
+    //%group="move"
+    export function walkTo(X:number, Y:number): void {
+        
+    }
+    
+}
+
+
+
+namespace spritePlus {
+
+}
